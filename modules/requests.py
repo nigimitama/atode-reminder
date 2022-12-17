f04a057b529bb98d2ffe4a3d1474b6f6ec98ffbf
mitama <nigimitama16@gmail.com>
2022-12-17 20:41:46 +0900

    Add message URL into remind message

--- modules/requests.py
+++ modules/requests.py
@@ -1,6 +1,7 @@
 from typing import Any
 from dataclasses import dataclass
 import urllib.request
+import urllib.parse
 import json
 
 
@@ -8,6 +9,22 @@ import json
 class Response:
     status_code: int
     body: Any
+
+
+def get(url: str, params: dict, headers: dict) -> Response:
+    request = urllib.request.Request(f"{url}?{urllib.parse.urlencode(params)}", headers=headers)
+    try:
+        with urllib.request.urlopen(request) as res:
+            body = res.read()
+            body = body.decode() if type(body) == bytes else body
+            status_code = 200
+    except urllib.error.HTTPError as err:
+        status_code = err.code
+        body = None
+    except urllib.error.URLError as err:
+        status_code = None
+        body = err.reason
+    return Response(status_code, body)
 
 
 def post(url: str, data: Any, headers: dict) -> Response:
